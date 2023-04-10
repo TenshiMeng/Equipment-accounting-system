39515fcda34cb668545e6169494f05e33b356380
Lebed <Lebed@DESKTOP-5EA35FK>
2023-04-10 23:51:23 +0300

    Добавьте файлы проекта.

---
+++ FirstDiplome/Core/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FirstDiplome.Core
+{
+    class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExcecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExcecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExcecute == null || _canExcecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ FirstDiplome/MWM/ViewModel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using FirstDiplome.Core;
+
+namespace FirstDiplome.MWM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand MenuViewCommand { get; set; }
+        public RelayCommand LoginViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+
+        public LoginViewModel LoginVM { get; set; }
+        public MenuViewModel MenuVM { get; set; }
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel() 
+        { 
+            HomeVM = new HomeViewModel();
+            MenuVM = new MenuViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand((o =>
+            {
+                CurrentView = HomeVM;
+            }));
+
+            MenuViewCommand = new RelayCommand((o =>
+            {
+                CurrentView = MenuVM;
+            }));
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoginVM = new LoginViewModel();
+            CurrentView = LoginVM;
+            LoginViewCommand = new RelayCommand((o =>
+            {
+                CurrentView = LoginVM;
+            }));
+        }
+    }
+}
